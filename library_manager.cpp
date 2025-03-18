f801832953388ea3e284ce3e0096403dbf1ec65d
Sagar Maheshwari <sagarmaheshwari650@gmail.com>
2025-03-18 20:51:29 +0530

    created a class library

--- library_manager.cpp
+++ library_manager.cpp
@@ -83,3 +83,98 @@ public:
         }
     }
 };
+class Library
+{
+private:
+    vector<Book> books;
+    vector<Member> members;
+
+public:
+    void addBook(Book book)
+    {
+        books.push_back(book);
+        cout << "Book \"" << book.getTitle() << "\" added to the library." << endl;
+    }
+
+    void addMember(Member member)
+    {
+        members.push_back(member);
+        cout << "Member \"" << member.getName() << "\" added to the library." << endl;
+    }
+
+    void displayBooks()
+    {
+        cout << "\nBooks in the Library:\n";
+        for (auto &book : books)
+        {
+            book.displayBook();
+        }
+    }
+
+    void displayMembers()
+    {
+        cout << "\nLibrary Members:\n";
+        for (auto &member : members)
+        {
+            member.displayMember();
+        }
+    }
+    void issueBook(int bookID, int memberID)
+    {
+        for (auto &book : books)
+        {
+            if (book.getID() == bookID)
+            {
+                if (!book.isBookIssued())
+                {
+                    for (auto &member : members)
+                    {
+                        if (member.getID() == memberID)
+                        {
+                            book.issueBook();
+                            member.issueBook();
+                            return;
+                        }
+                    }
+                    cout << "Member ID not found." << endl;
+                    return;
+                }
+                else
+                {
+                    cout << "Book is already issued!" << endl;
+                    return;
+                }
+            }
+        }
+        cout << "Book ID not found." << endl;
+    }
+    void returnBook(int bookID, int memberID)
+    {
+        for (auto &book : books)
+        {
+            if (book.getID() == bookID)
+            {
+                if (book.isBookIssued())
+                {
+                    for (auto &member : members)
+                    {
+                        if (member.getID() == memberID)
+                        {
+                            book.returnBook();
+                            member.returnBook();
+                            return;
+                        }
+                    }
+                    cout << "Member ID not found." << endl;
+                    return;
+                }
+                else
+                {
+                    cout << "Book is not issued!" << endl;
+                    return;
+                }
+            }
+        }
+        cout << "Book ID not found." << endl;
+    }
+};
